6e632f5bb457b5c44656345db78da222f0e2459e
Harsh Singh <hi.harsh@protonmail.ch>
2021-04-08 14:43:26 -0400

    Get Snipped Link

---
+++ src/sniplink.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export class GetSnippedLink {
+  id: string
+
+  constructor(id: string) {
+    this.id = id
+  }
+
+  public async getPaste() {
+    const response = await axios.get(
+      `https://beta.sniplink.net/api/v1/link/${this.id}`
+    )
+
+    return response.data
+  }
+}
+
+const SnippedLink = new GetSnippedLink('j1W')
+console.log(SnippedLink)
