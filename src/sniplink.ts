3e355856c93c309910296f7f05477fe739bfe588
Harsh Singh <hi.harsh@protonmail.ch>
2021-04-08 21:26:37 -0400

    Create Sniplink

--- src/sniplink.ts
+++ src/sniplink.ts
@@ -14,3 +14,24 @@ export class GetSnippedLink {
     return response.data
   }
 }
+
+export class CreateSnippedLink {
+  expirationTime: string
+  value: string
+
+  constructor(expirationTime: string, value: string) {
+    this.expirationTime = expirationTime
+    this.value = value
+  }
+
+  public async createSnippedLink() {
+    const response = await axios.post(
+      'https://beta.sniplink.net/api/v1/link/',
+      {
+        expirationTime: this.expirationTime,
+        value: this.value,
+      }
+    )
+    return response.data
+  }
+}
